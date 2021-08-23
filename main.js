a8f148ae969d1140ba715097359c2787193e571e
Liel <liel.izcha321@gmail.com>
2021-08-23 21:19:30 +0300

    -6

--- main.js
+++ main.js
@@ -19,13 +19,19 @@ formOne.onsubmit = function (event) {
     //   exeresie 2
     examDate: new Date(),
   };
-  Patient.push(newObject);
+  if (checkId(Patient, inputId)) {
+    alert("already exist..");
+  } else {
+    Patient.push(newObject);
+  }
   console.log(Patient);
+
+  displayData(Patient);
 };
 var Patient = [];
 // exeresie 3
 
-function name(array, idNewUser) {
+function checkId(array, idNewUser) {
   for (let i = 0; i < array.length; i++) {
     if (array[i].id == idNewUser) {
       return true;
@@ -33,3 +39,23 @@ function name(array, idNewUser) {
   }
   return false;
 }
+// exeresie 4
+function displayData(myArray) {
+  var pher = document.getElementById("phar");
+  pher.innerHTML = "";
+  for (var i = 0; i < myArray.length; i++) {
+    pher.innerHTML += myArray[i].firstName + "<br>";
+    pher.innerHTML += myArray[i].LastName;
+    pher.innerHTML += myArray[i].YearOfBirth;
+  }
+}
+function displayData(myArray) {
+  var p = document.getElementById("para");
+  p.innerHTML = "";
+  for (let i = 0; i < myArray.length; i++) {
+    // exeresie 5
+    p.innerHTML += `name: "${myArray[i].Name}" ,last name: "${myArray[i].lastName}" ,birth-day: "${myArray[i].birthDay}" , id: "${myArray[i].id}", city: "${myArray[i].city}"`;
+  }
+
+}
+// exeresie 6 
